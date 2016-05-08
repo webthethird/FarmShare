ba93ad4a9ed1dd1bffc90bed1757c45473d66911
libertect <bodellarch@yahoo.com>
2016-05-08 16:35:29 -0400

    clean up

--- contracts/FarmShare.sol
+++ contracts/FarmShare.sol
@@ -32,14 +32,15 @@ contract FarmShare {
         Done
     }
 
+	//constructor
 	function FarmShare () {
 		_treasury = msg.sender;  
 		_community = msg.sender;
 		_totalMinted = 0;
 		_iniMemberTokens = 10;
 		baseUnits = 100;
-    	tokenName = "shares";
-    	symbol = "FS";
+    		tokenName = "shares";
+    		symbol = "FS";
 		communityName = "FarmShare";
 		totalTasks = 0;
 	}
@@ -67,6 +68,7 @@ contract FarmShare {
         string name;
         string description;
         uint price;
+        uint quantity;
         Member seller;
         Member buyer;
     }    
@@ -78,9 +80,47 @@ contract FarmShare {
 	event Transfer(uint _amount, address indexed _from, address indexed _to, uint _timeStampT);
 	event NewMember(address indexed _member, string _alias);
 	event OldMember(address indexed _exMember, string _exAlias);
-    event NewTask(uint id, string name, string description);    
-    event NewProduct(uint id, string name, string description);
+    event NewTask(uint id, string name, string description, uint reward);    
+    event NewProduct(uint id, string name, string description, uint price, uint quantity);
+    
+    function transfer (address _payee, uint _payment) {
+		uint _available = members[msg.sender].balance;
+		uint _amountTokens = uint(_payment); 
+		if (_available > _amountTokens) {
+			members[msg.sender].balance -= _amountTokens;
+			members[_payee].balance += _amountTokens;
+			// @notice apply community tax and send it to the Community account
+			members[_payee].balance -= _amountTokens * _communityTax/100;
+			members[_community].balance += _amountTokens * _communityTax/100;
+			Transfer(_payment, msg.sender, _payee, now);
+		}
+	}
 	
+	function getParameters() constant returns (address getTreasury, address getCommunity, uint getIniMemberTokens, uint getIniMemberReputation, string getName, string getSymbol, string getCommunityName) {
+		getTreasury = _treasury;
+		getCommunity = _community;
+		getIniMemberTokens = _iniMemberTokens;
+		getName = tokenName;
+		getSymbol = symbol;
+		getCommunityName = communityName;
+	}
+
+	function newParameters (int _newCommunityTax, uint _newRewardRate, uint _newIniTokens, uint _newIniR) {
+		if (msg.sender != _treasury) return;
+		_iniMemberTokens = _newIniTokens;
+	}
+	
+	function newCommunity (address _newCommunity) {
+		if (msg.sender != _treasury) return;
+		_community = _newCommunity;
+	}
+	
+	function newTreasury (address _newTreasury) {
+		if (msg.sender != _community) return;
+		_treasury = _newTreasury;
+	}
+	
+	//membership
 	function acceptMember (address _newMember, string _newName) {
         if (msg.sender != _community) return;
         members[_newMember].account = _newMember;
@@ -99,44 +139,16 @@ contract FarmShare {
         OldMember(_oldMember, _oldName);
     }
     
-    function transfer (address _payee, uint _payment) {
-		uint _available = members[msg.sender].balance;
-		uint _amountTokens = uint(_payment); 
-		if (_available > _amountTokens) {
-			members[msg.sender].balance -= _amountTokens;
-			members[_payee].balance += _amountTokens;
-			// @notice apply community tax and send it to the Community account
-			members[_payee].balance -= _amountTokens * _communityTax/100;
-			members[_community].balance += _amountTokens * _communityTax/100;
-			Transfer(_payment, msg.sender, _payee, now);
-		}
-	}
+    function getMemberInfo(uint id) constant returns(string name, address account, uint balance, uint reputation, uint tasks) {
+        string memberName = members[memberArray[id]].name;
+        address memberAcct = members[memberArray[id]].account;
+        uint memberBal = members[memberArray[id]].balance;
+        uint memberRep = members[memberArray[id]].reputation;
+        uint memberTasks = members[memberArray[id]].tasksCompleted;
+        return(memberName, memberAcct, memberBal, memberRep, memberTasks);
+    }
 	
-	function getParameters() constant returns (address _getTreasury, address _getCommunity, uint _getIniMemberTokens, uint _getIniMemberReputation, uint _getExchange, string getName, string getSymbol, string getCommunityName, uint getBaseUnits) {
-		_getTreasury = _treasury;
-		_getCommunity = _community;
-		_getIniMemberTokens = _iniMemberTokens;
-		getName = tokenName;
-		getSymbol = symbol;
-		getCommunityName = communityName;
-		getBaseUnits = baseUnits;
-	}
-
-	function newParameters (int _newCommunityTax, uint _newRewardRate, uint _newIniTokens, uint _newIniR) {
-		if (msg.sender != _treasury) return;
-		_iniMemberTokens = _newIniTokens;
-	}
-	
-	function newCommune (address _newCommunity) {
-		if (msg.sender != _treasury) return;
-		_community = _newCommunity;
-	}
-	
-	function newTreasury (address _newTreasury) {
-		if (msg.sender != _community) return;
-		_treasury = _newTreasury;
-	}
-	
+	//tasks
     function newTask(string _name, string _description, uint _reward) public returns (uint32 taskId) {
         Task newTask = tasks[totalTasks];
         newTask.owner = members[msg.sender];
@@ -148,24 +160,8 @@ contract FarmShare {
         totalTasks++;
         taskArray.push(totalTasks);
         
-        NewTask(totalTasks, _name, _description);
+        NewTask(totalTasks, _name, _description, _reward);
         return(totalTasks);
-    }
-    
-    function setTaskReward(uint _taskId, uint _reward, string _description) returns (bool success) {
-        Task task = tasks[_taskId];
-        if (task.owner.account == msg.sender) {
-            task.reward = _reward;
-        }
-    }
-    
-    function getTasks() constant returns(uint32[] tList) {
-        return(taskArray);
-    }
-    
-    function getTaskName(uint id) constant returns(string tName) {
-        string name = tasks[id].name;
-        return(name);
     }
     
     function getTaskInfo(uint id) constant returns(string name, string description, uint reward, address owner, address volunteer, TaskStatus status){
@@ -175,33 +171,21 @@ contract FarmShare {
         uint taskReward = task.reward;
         address taskOwner = task.owner.account;
         address taskVolunteer = task.volunteer.account;
-        var taskStatus = task.status;
+        TaskStatus taskStatus = task.status;
         return (taskName, taskDescription, taskReward, taskOwner, taskVolunteer, taskStatus);
     }
     
-    function getMemberName(uint id) constant returns(string mName) {
-        string name = members[memberArray[id]].name;
-        return(name);
-    }
-    
-    function getMemberBalance(address addr) constant returns(uint mBal){
-        uint bal = members[addr].balance;
-        return (bal);
-    }
-    
-    function getTaskStatus(uint id) constant returns(string status) {
-        var taskStatus = tasks[id].status;
-        if (taskStatus == TaskStatus.New) {
-            return("New");
-        } else if (taskStatus == TaskStatus.InProgress) {
-            return("In Progress");
-        } else if (taskStatus == TaskStatus.Completed) {
-            return("Completion Awaiting Confirmation");
-        } else if (taskStatus == TaskStatus.Done) {
-            return("Done");
-        }
-    }
-    
+    // function getOpenTasks() constant returns(uint[] taskList) {
+    //     uint[] tList;
+    //     var list = taskArray;
+    //     for (uint i = 0; i < list.length; i++){
+    //         if (tasks[i].status == TaskStatus.New) {
+    //             tList.push(i);
+    //         }
+    //     }
+    //     return (tList);
+    // }
+
     function acceptTask(uint id) public returns (bool success) {
         Task task = tasks[id];
         if (task.status == TaskStatus.New) {
@@ -227,99 +211,39 @@ contract FarmShare {
         }
         return (taskStatus);
     }
-    
-    function newSeller(string _name) public returns(uint sellerId) {
-        Member newSeller = members[msg.sender];
-        newSeller.name = _name;
-        newSeller.account = msg.sender;
-        sellerArray.push(newSeller.account);
-        
-        return(sellerArray.length);
-      
-  }
 
-    function newBuyer(string _name) public returns(uint buyerId) {
-        Member newBuyer = members[msg.sender];
-        newBuyer.name = _name;
-        newBuyer.account = msg.sender;
-        buyerArray.push(newBuyer.account);
-        
-        return(buyerArray.length);
-  }
-
-    function newProduct(string _name, string _description, uint256 _price) public returns (uint32 taskId) {
+    //market
+    function newProduct(string _name, string _description, uint256 _price, uint _quantity) public returns (uint32 prodId) {
         Product newProduct = products[totalProducts];
         newProduct.seller = members[msg.sender];
         newProduct.name = _name;
         newProduct.description = _description;
-        // newProduct.status = ProductStatus.New;
         newProduct.price = _price;
+        newProduct.quantity = _quantity;
         
         totalProducts++;
         // productArray.push(totalProducts);
         
-        NewProduct(totalProducts, _name, _description);
+        NewProduct(totalProducts, _name, _description, _price, _quantity);
         return(totalProducts);
   }
-  
-    function setProductPrice(uint _productId, uint _price, string _description) returns (bool success) {
-        Product product = products[_productId];
-        if (product.seller.account == msg.sender) {
-            product.price = _price;
-        }
-    }
-        
-    function getSellers() constant returns(address[] sList) {
-        return(sellerArray);
+    
+    function getProductInfo(uint id) constant returns(string name, string seller, string description, uint price, uint quantity) {
+        Product prod = products[id];
+        name = prod.name;
+        seller = prod.seller.name;
+        description = prod.description;
+        price = prod.price;
+        quantity = prod.quantity;
+        return (name, seller, description, price, quantity);
     }
     
-    function getProducts() constant returns(uint32[] pList) {
-        return(productArray);
-    }
-    
-    function getProductNames() constant {
-        var list = productArray;
-        for (uint i = 0; i < list.length; i++) {
-            getProductName(i);
-        }
-    }
-    
-    function getSellerNames() constant {
-        var list = sellerArray;
-        for (uint i = 0; i < list.length; i++) {
-            getSellerName(i);
-        }
-    }
-    
-    function getProductName(uint id) constant returns(string pName) {
-        string name = products[id].name;
-        return(name);
-    }
-    
-    function getSellerName(uint id) constant returns(string sName) {
-        string name = members[sellerArray[id]].name;
-        return(name);
-    }
-    
-    function getProductDescription(uint id) constant returns(string pDescription) {
-        string description = products[id].description;
-        return(description);
-    }
-
-    function getProductPrice(uint id) constant returns(uint price) {
-        return(products[id].price);
-    }
-    
-    function getProductSellerName(uint id) constant returns(string seller) {
-        string name = products[id].seller.name;
-        return(name);
-    }
-    
-    function purchaseProduct(uint id) {
+    function purchaseProduct(uint id, uint quant) {
         Product product = products[id];
-        if (msg.sender != product.seller.account && members[msg.sender].balance >= product.price) {
-           members[msg.sender].balance -= product.price;
-           members[product.seller.account].balance += product.price;
+        if (msg.sender != product.seller.account && members[msg.sender].balance >= product.price*quant && product.quantity > quant) {
+           members[msg.sender].balance -= product.price*quant;
+           members[product.seller.account].balance += product.price*quant;
+           product.quantity -= quant;
         }
     }
 }
